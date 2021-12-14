f643c221728c63ae123eb4babf0d1323d8256a14
zhangting <273664273@qq.com>
2021-12-14 14:46:51 +0800

    上传图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="upload-box">
+    <!-- 怎么动态绑定类名??????????????? -->
+    <!-- 三种方法 1.直接绑定   2.三元表达式绑定   3.对象绑定    下边的class就是用的对象绑定 -->
     <el-upload
       :on-preview="preview"
       :file-list="fileList"
@@ -7,22 +9,31 @@
       :on-change="handleChange"
       :http-request="handleRequest"
       :before-upload="beforeUpload"
-      :class="hidePlus"
+      :class="{ hidePlus: isLen }"
       list-type="picture-card"
       action="#"
     >
       <i class="el-icon-plus" />
     </el-upload>
 
-    <el-dialog width="600px" top="8vh" title="图片预览" :visible.sync="showDialog">
+    <el-dialog
+      width="600px"
+      top="8vh"
+      title="图片预览"
+      :visible.sync="showDialog"
+    >
       <img width="100%" :src="imgUrl" alt="">
     </el-dialog>
   </div>
 </template>
 
 <script>
-
-export default ({
+import COS from 'cos-js-sdk-v5' // 导入腾讯云的包(sdk)
+const cos = new COS({
+  SecretId: 'AKIDtb2C6j5AyWnnWzB6kDPKyLRGn33nG8k5', // 身份识别 ID
+  SecretKey: '5tQhhPq1vYu0oNfgRv84k9H7hz3qMccL' // 身份密钥
+})
+export default {
   // 这里如果不写name属性,在全局注册组件的时候,取其name属性,就会报错,Cannot read properties of undefined (reading ‘toLowerCase‘)
   name: 'ImageUpload',
   data() {
@@ -37,7 +48,10 @@ export default ({
     }
   },
   computed: {
-
+    isLen() {
+      // 这里会返回true 或者false 在上边动态绑定类名的时候,就根据true或者false来判断类名是否生效
+      return this.fileList.length >= 1
+    }
   },
   methods: {
     // 上传前的类型以及大小的验证
@@ -53,25 +67,76 @@ export default ({
         return false
       }
       // 验证通过
-      console.log('验证通过')
+      // console.log('验证通过')
       return
     },
     // 手动提交
     handleRequest(params) {
-      console.log('handleRequest', params)
+      console.log('handleRequest', params.file)
+      // 在手动提交的时候,要将图片提交到第三方平台(腾讯云),再将腾讯云返回的地址传递给后台
+
+      if (params.file) {
+        // 执行上传操作
+        cos.putObject(
+          {
+            Bucket: 'my-hr-1308732703' /* 存储桶名称 */,
+            Region: 'ap-beijing' /* 存储桶所在地域，必须字段 */,
+            Key: params.file.name /* 文件名 */,
+            StorageClass: 'STANDARD', // 上传模式, 标准模式（默认即可）
+            Body: params.file, // 上传文件对象
+            onProgress: (progressData) => {
+              console.log(JSON.stringify(progressData))
+            }
+          },
+          (err, data) => {
+            console.log(err, data)
+            // 上传的结果：如果err有值，证明上传失败了，如果err是null表示上传成功
+            // data表示上传成功后，腾讯云返回的图片地址,Location
+            // 在vue变量fileList中和腾讯云返回的图片信息中对比,是不是同一张,是同一张的话,就能确定是已经在腾讯云上传成功,并且返回了blob地址(本地预览地址),但是并不能把这恶地址发给后台,发给后台的地址需要自己进行拼接,因为
+            if (err) {
+              this.$message.error('图片上传失败')
+              return
+            }
+            // 图片上传成功 data不为空
+            if (data.statusCode === 200) {
+              // 在vue变量fileList中和腾讯云返回的图片信息中对比,是不是同一张,是同一张的话,就能确定是已经在腾讯云上传成功
+              // 找到了的话,就给找到数据的status赋值为success,也就是给右上角显示绿色的对勾
+              // 数组常见方法 find filter some
+              const fileInfo = this.fileList.find((item) => {
+                console.log(item.uid, params.file.uid)
+                // 如果此处不return的话,,fileInfo就是undefined  只有定义,没有赋值
+                // 箭头函数,如果函数体只有一行的话,是可以省略{} 和return的,默认是会return的
+                return item.uid === params.file.uid
+              })
+              console.log(fileInfo)
+              if (fileInfo) {
+                // 之前action自动上传的时候,当文件上传成功之后,,,图片右上角会有一个绿色的对勾,,,这里涉及到一个状态   on-change这个属性上绑定的函数的参数file中 status: "ready"  当图片上传成功之后,会将状态置为success
+                // 如果fileInfo非空,并且是上传到腾讯云是成功的,就将status
+                // find返回的是满足条件的第一个元素,,,现在是数组包对象,所以fileInfo是一个对象,不是数组
+                // fileInfo[0].status = 'success'
+                fileInfo.status = 'success'
+                // 将腾讯云返回的图片地址赋值给vue变量fileList 传递给后端
+                this.fileList.url = 'https://' + fileInfo.Location
+              }
+            } else {
+              this.$message.error('图片上传失败')
+            }
+          }
+        )
+      }
     },
     //
     handleChange(file, fileList) {
       // file.raw.uid  file.size   file.name
       console.log('handleChange', file, fileList)
-    },
-    // 成功做的处理
-    handleSuccess() {
 
+      this.fileList = fileList
     },
     // 移除图片
     handleRemove(file, fileList) {
-      console.log('handleRemove', file, fileList)
+      // console.log('handleRemove', file, fileList)
+
+      this.fileList = fileList
     },
     // 预览图片
     preview(file) {
@@ -80,14 +145,27 @@ export default ({
       this.showDialog = true
     }
   }
-})
+}
 </script>
 
 <style scoped lang="scss">
-// el-upload--picture-card 是动态生成的
+// el-upload--picture-card 是动态生成的,所以要用 深度选择器来穿透
+// scss中的深度选择器需要使用 ::v-deep  /deep/
 .upload-box {
-  .hidePlus{
+  // hidePlus限制+ 号,只允许上传一张照片
+  // 但是这个组件是允许上传多张图片的,到时候,允许上传的图片张数由父组件传过来
+  // 当加上这个类之后,就可以隐藏 + 号,但是当把原有的一张图片删除时候,就应该将 + 显示出来
+  //  文件列表移除文件时的钩子 on-remove 上绑定的方法 handleRemove 第二个参数是删除文件之后的文件列表
+  // 将删除之后的 文件列表赋值给vue变量fileList,当vue变量fileList变化之后,就会触发计算属性,,,
+  // 所以,hidePlus这个类就不会生效
 
+  // 当删除了原来的照片之后,再自行添加照片,也就是手动提交照片,会触发http-request,
+  // 在http-request上绑定了函数handleRequest有一个参数,这个参数有一个属性file,file里边有uid name size type  不知道handleRequest带的形参有什么,可以打印argumens,,绑定一个点击事件
+  // 要将这个照片的地址放在vue变量的fileList中去,不然后边没办法将数据提交给后台
+  ::v-deep .hidePlus {
+    .el-upload--picture-card {
+      display: none;
+    }
   }
 }
 </style>
